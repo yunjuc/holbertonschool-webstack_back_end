fe435ac24f0ef23caeee5ab9b44a67e81a36da7c
Yunju Chen <info@yunjuchen.net>
2019-03-01 07:27:23 +0000

    Add advanced tasks

--- 0x00-restful_api_users/models/base_model.py
+++ 0x00-restful_api_users/models/base_model.py
@@ -19,3 +19,49 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = str(datetime.utcnow())
         self.updated_at = str(datetime.utcnow())
+
+    @classmethod
+    def all(cls):
+        '''return all instances of a class'''
+        from models import db_session
+
+        objs = db_session.query(cls).order_by(cls.created_at.asc()).all()
+        return objs
+
+    @classmethod
+    def count(cls):
+        '''count number of instances of a class'''
+        from models import db_session
+
+        n = db_session.query(cls).count()
+        return n
+
+    @classmethod
+    def get(cls, id):
+        '''return all instances of a class'''
+        from models import db_session
+
+        if id is None or type(id) != str:
+            return None
+        obj = db_session.query(cls).filter(cls.id == id).first()
+        if obj is None:
+            return None
+        return obj
+
+    @classmethod
+    def first(cls):
+        '''return an instanc by'''
+        from models import db_session
+
+        obj = db_session.query(cls).order_by(cls.created_at.asc()).first()
+        if obj is None:
+            return None
+        return obj
+
+    @classmethod
+    def last(cls):
+        '''return last instance of a class'''
+        from models import db_session
+
+        obj = db_session.query(cls).order_by(cls.created_at.desc()).first()
+        return obj
