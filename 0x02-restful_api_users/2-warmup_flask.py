06ae3d53665b391c9558690b29a7a492071a3abc
Yunju Chen <info@yunjuchen.net>
2019-03-18 14:38:46 -0700

    Update 2-warmup_flask.py

--- 0x02-restful_api_users/2-warmup_flask.py
+++ 0x02-restful_api_users/2-warmup_flask.py
@@ -5,18 +5,17 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 host = os.getenv('HBNB_API_HOST')
 port = os.getenv('HBNB_API_PORT')
 
 
-@app.route("/")
+@app.route("/", strict_slashes = False)
 def get_index():
     '''get_index() - GET /'''
     return "Holberton School"
 
 
-@app.route("/c")
+@app.route("/c", strict_slashes = False)
 def get_c():
     '''get_c() - GET /c'''
     return "C is fun!"
