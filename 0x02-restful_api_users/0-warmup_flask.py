4a1f5caa04f606b67c71bc7fb98b2ebbd89c9fdd
Yunju Chen <info@yunjuchen.net>
2019-03-18 13:03:48 -0700

    Update 0-warmup_flask.py

--- 0x02-restful_api_users/0-warmup_flask.py
+++ 0x02-restful_api_users/0-warmup_flask.py
@@ -4,10 +4,9 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     '''GET index'''
     return "Holberton School"
