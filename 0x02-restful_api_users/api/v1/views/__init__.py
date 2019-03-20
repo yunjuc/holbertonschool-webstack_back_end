6bd41d6beb80f906f24c0097646955c316483e37
Yunju Chen <info@yunjuchen.net>
2019-03-20 16:09:24 -0700

    Update __init__.py

--- 0x02-restful_api_users/api/v1/views/__init__.py
+++ 0x02-restful_api_users/api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''app_views module'''
+'''__init__.py -- app_views module'''
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/')
 from api.v1.views.index import *
