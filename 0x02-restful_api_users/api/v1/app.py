85b9a39a75a372beb727118df3c5022e4d11818d
Yunju Chen <info@yunjuchen.net>
2019-03-20 16:36:46 -0700

    Update app.py

--- 0x02-restful_api_users/api/v1/app.py
+++ 0x02-restful_api_users/api/v1/app.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-'''app.py -- app module documentation'''
+"""
+app.py - app file sdklfa sklflasdf lasdflkj sadflk asdfasjk fd
+"""
 import os
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from models import db_session
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-host = os.getenv('HBNB_API_HOST')
-port = int(os.getenv('HBNB_API_PORT'))
-
-
-@app.teardown_appcontext
-def close_db(error):
-    '''close_db() - close database after api call'''
-    db_session.close()
 
 
 @app.errorhandler(404)
-def error_page(e):
-    '''error_page() - handle 404 not found error'''
+def page_not_found(e):
+    """ page_not_found() - handle 404 error """
     return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == '__main__':
-    app.run(host=host, port=port)
+if __name__ == "__main__":
+    envHost = os.environ.get('HBNB_API_HOST')
+    envPort = int(os.environ.get('HBNB_API_PORT'))
+    app.run(host=envHost, port=envPort)
+    import doctest
+    doctest.testmod(verbose=True)
