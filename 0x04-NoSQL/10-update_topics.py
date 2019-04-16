84691437596f26e01c916e771201e04912157d0d
Yunju Chen <info@yunjuchen.net>
2019-04-16 19:41:13 +0000

    Update many

--- 0x04-NoSQL/10-update_topics.py
+++ 0x04-NoSQL/10-update_topics.py
@@ -5,5 +5,5 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     '''update_topics() - update documenet data'''
-    return mongo_collection.update({'name': name}, {'$set':
-                                   {'topics': topics}})
+    return mongo_collection.update_many({'name': name}, {'$set':
+                                        {'topics': topics}})
